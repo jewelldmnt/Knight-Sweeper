fd443432015c1e1ec3d57c410fbee4e24006d486
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2024-06-24 12:00:10 +0800

    updated the movements

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ fps = 60
 ##########################################################################
 # GAME VARIABLES
 ##########################################################################
+isSecondIter = False
 red_pieces = ['knight'] + ['apple'] * 31
 red_locations = [(0,0), (1,0), (2,0), (3,0),
                  (0,1), (1,1), (2,1), (3,1),
@@ -23,6 +24,10 @@ red_locations = [(0,0), (1,0), (2,0), (3,0),
                  (0,5), (1,5), (2,5), (3,5),
                  (0,6), (1,6), (2,6), (3,6),
                  (0,7), (1,7), (2,7), (3,7)]
+red_apples_locations = red_locations[1:]
+red_knight_location = (0,0)
+extra_red_apple = {}
+
 green_pieces = ['apple'] * 31
 green_pieces = green_pieces + ['knight']
 green_locations = [(4,0), (5,0), (6,0), (7,0),
@@ -33,11 +38,14 @@ green_locations = [(4,0), (5,0), (6,0), (7,0),
                  (4,5), (5,5), (6,5), (7,5),
                  (4,6), (5,6), (6,6), (7,6),
                  (4,7), (5,7), (6,7), (7,7)]
+green_apples_locations = green_locations[:-2]
+green_knigh_location = (7,7)
+extra_green_apple = {}
 
 captured_pieces_red = []
 captured_pieces_green = []
 
-# 0 - red turn no selection; 1 - red turn piece selected; 2 - green turn no selection; 3 - green turn no selection
+# 0 - red turn no selection; 1 - red turn piece selected; 2 - green turn no selection; 3 - green turn piece selected
 turn_step = 0
 selection = 100
 valid_moves = []    
@@ -90,10 +98,16 @@ powerups_list = ['golden apple', 'poisoned apple']
 # check variables / flashing the counter
 
 
-
-
-# draw main game board
 def draw_board():
+    """
+    Draws the main game board, including grid lines, status text, and board borders.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
     for i in range(32):
         pygame.draw.rect(screen, 'gray', [0, 800, WIDTH, 100])
         pygame.draw.rect(screen, 'gold', [0, 800, WIDTH, 100], 5)
@@ -107,6 +121,15 @@ def draw_board():
 
 
 def draw_pieces():
+    """
+    Draws the game pieces on the board, including highlighting the selected piece.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
     for i in range(len(red_pieces)):
         index = piece_list.index(red_pieces[i])
         x, y = red_locations[i]
@@ -114,8 +137,9 @@ def draw_pieces():
             screen.blit(red_images[index], (x * 100 + 10, y * 100 + 10))
         else:
             screen.blit(red_images[index], (x * 100 + 22, y * 100 + 30))
-        if turn_step < 2 and selection == i:
-            pygame.draw.rect(screen, 'red', [x * 100 + 1, y * 100 + 1, 100, 100], 2)
+        if turn_step < 2 and selection == i and red_pieces[i] == 'knight':
+            pygame.draw.rect(screen, 'yellow', [red_locations[i][0] * 100 + 1, red_locations[i][1] * 100 + 1, 
+                             100, 100], 2)
 
     for i in range(len(green_pieces)):
         index = piece_list.index(green_pieces[i])
@@ -124,29 +148,171 @@ def draw_pieces():
             screen.blit(green_images[index], (x * 100 + 10, y * 100 + 10))
         else:
             screen.blit(green_images[index], (x * 100 + 22, y * 100 + 30))
-        if turn_step >= 2 and selection == i:
-            pygame.draw.rect(screen, 'blue', [x * 100 + 1, y * 100 + 1, 100, 100], 2)
+        if turn_step >= 2 and selection == i and green_pieces[i] == 'knight':
+            pygame.draw.rect(screen, 'green', [green_locations[i][0] * 100 + 1, green_locations[i][1] * 100 + 1, 
+                             100, 100], 5)
+
+# check valid knight moves
+def check_knight(position, color):
+    moves_list = []
+    if color == 'red':
+        enemies_list = green_locations
+        friends_list = red_locations
+    else:
+        enemies_list = red_locations
+        friends_list = green_locations
+    
+    
+    # 8 squares to check for knight, they can go two squares in one direction and one in another
+    targets = [(1, 2), (1, -2), (2, 1), (2, -1), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
+    for i in range(8):
+        target = (position[0] + targets[i][0], position[1] + targets[i][1])
+        if (target in friends_list or target in enemies_list) and 0 <= target[0] <= 7 and 0 <= target[1] <= 7:
+            moves_list.append(target)
+    return moves_list
+
+# check for valid moves for just selected piece
+def check_valid_moves():
+    if turn_step < 2: 
+        options_list = red_options
+    else:
+        options_list = green_options
+    valid_options = options_list[selection]
+    return valid_options
+
+
+def check_options(pieces, locations, turn):
+    moves_list = []
+    all_moves_list = []
+    for i in range(len(pieces)):
+        location = locations[i]
+        piece = pieces[i]
+        if piece == 'knight':
+            moves_list = check_knight(location, turn)
+        all_moves_list.append(moves_list)
+    return all_moves_list
 
 
 
-        
-        
+# draw valid moves
+def draw_valid(moves):
+    color = 'blue'
+    for i in range(len(moves)):
+        pygame.draw.circle(screen, color, (moves[i][0] * 100 + 50, moves[i][1] * 100 + 50), 5)
+    
+    
 ##########################################################################
 # MAIN LOOP
 ##########################################################################
+green_options = check_options(green_pieces, green_locations, 'green')
+red_options = check_options(red_pieces, red_locations, 'red')
+print(f"red options: {red_options}")
+print(f"green options: {green_options}")
+
+
 run = True
 while run:
     timer.tick(fps)
     screen.fill('light gray')
     draw_board()
     draw_pieces()
-    
+
+    if selection != 100:
+        valid_moves = check_valid_moves()
+        draw_valid(valid_moves)
+
+            
     # event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            x_coord = event.pos[0] // 100
+            y_coord = event.pos[1] // 100
+            click_coords = (x_coord, y_coord)
             
+            
+            if turn_step <= 1: # red to move              
+                if click_coords in red_locations:
+                    selection = red_locations.index(click_coords)
+                    print(f'red_pieces[selection]: {red_pieces[selection]}')
+                    if red_pieces[selection] == 'knight':  # Only allow selection if it's a knight
+                        if turn_step == 0:
+                            turn_step = 1
+                            
+                if click_coords in valid_moves and selection != 100:
+                    red_locations[selection] = click_coords
+                    print(f'red_locations[selection]: {red_locations[selection]}')
+                    
+                    if len(extra_red_apple) != 0:
+                        index = list(extra_red_apple.keys())[0]
+                        print(f'index: {index}')
+                        red_pieces.insert(index, 'apple')
+                        red_locations.insert(index, extra_red_apple[index])
+                        extra_red_apple = {}
+                        
+                    if click_coords in green_locations:
+                        green_piece = green_locations.index(click_coords)
+                        captured_pieces_red.append(green_pieces[green_piece])
+                        green_pieces.pop(green_piece)
+                        green_locations.pop(green_piece)
+
+                    if click_coords in red_locations:
+                        red_piece = red_locations.index(click_coords)
+                        extra_red_apple = {red_piece: click_coords}
+                        red_pieces.pop(red_piece)
+                        red_locations.pop(red_piece)
+                    
+                    red_locations[0] = click_coords
+                    red_pieces[0] = 'knight'
+                    print(f'red pieces: {red_pieces}')
+                    
+                    green_options = check_options(green_pieces, green_locations, 'green')
+                    red_options = check_options(red_pieces, red_locations, 'red')
+                    turn_step = 2
+                    selection = 100
+                    isSecondIter = True
+                    valid_moves = []
+                    
+                    
+            if turn_step > 1: # green to move
+
+                if click_coords in green_locations:
+                    selection = green_locations.index(click_coords)
+                    if green_pieces[selection] == 'knight':  # Only allow selection if it's a knight
+                        if turn_step == 2:
+                            turn_step = 3
+                                                        
+                if click_coords in valid_moves and selection != 100:
+                    green_locations[selection] = click_coords
+        
+                    if len(extra_green_apple) != 0:
+                        index = list(extra_green_apple.keys())[0]
+                        green_pieces.insert(index, 'apple')
+                        green_locations.insert(index, extra_green_apple[index])
+                        extra_green_apple = {}
+                    
+                    if click_coords in red_locations:
+                        red_piece = red_locations.index(click_coords)
+                        captured_pieces_green.append(red_pieces[red_piece])
+                        red_pieces.pop(red_piece)
+                        red_locations.pop(red_piece)                      
+                        
+                    if click_coords in green_locations:
+                        green_piece = green_locations.index(click_coords)
+                        extra_green_apple = {green_piece: click_coords}
+                        green_pieces.pop(green_piece)
+                        green_locations.pop(green_piece)
+                    
+                    green_locations[-1] = click_coords
+                    green_pieces[-1] = 'knight'
+                    
+                    green_options = check_options(green_pieces, green_locations, 'green')
+                    red_options = check_options(red_pieces, red_locations, 'red')
+                    turn_step = 0
+                    selection = 100
+                    valid_moves = []
+                
+                
     pygame.display.flip()
 pygame.quit()
-        
-        
